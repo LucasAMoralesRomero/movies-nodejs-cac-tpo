cf22f82d57c6b58e60e2385753fc9da4a5e4b45a
Lucas Agustin Morales Romero <moralesromerolucasagustin@gmail.com>
2024-05-21 21:13:49 -0300

    now is working, all the movies are displaying

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function getDataMovies(){
         .then(response => response.json())
         .then(json => {
             data = json;
+            displayMovies();
     })
     //capturamos el posible error y lo mostramos en consola
     .catch(error=> console.error('Error al cargar el archivo JSON: ', error));
@@ -13,16 +14,21 @@ function getDataMovies(){
 
 //funcion para mosrtar las peliculas
 function displayMovies(){
-    var templateMovieCard = '';
+    var templateMovieCard = ''
+    console.log(data)
     data.forEach(event => {
         //le asignaoms a la tmeplatecard cada vez q iterer una carta
         templateMovieCard +=
       `
-      <img src="${data.imagen1}" alt="">
-      <h4 class="movieTitle"></h4>
-      <h5 class="genero"></h5>
-      <h5 class="anio"></h5>
-      <p class="movieDescription"></p>
+      <div>
+      <img class="contenedor-img" src="${event.imagen1}" alt=""
+      onmouseover="this.src='${event.imagen2}'"
+      onmouseout="this.src='${event.imagen1}'">
+      <h4 class="movieTitle">${event.pelicula}</h4>
+      <h5 class="genero">${event.genero1} ${event.genero2} ${event.genero3}</h5>
+      <h5 class="anio">${event.anio}</h5>
+      <p class="movieDescription">${event.descripcion}</p>
+      </div>
       `
     })
     document.getElementById('movieCard').innerHTML = templateMovieCard;
