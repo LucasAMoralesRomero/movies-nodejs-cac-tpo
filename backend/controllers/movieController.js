6ee6a4546b2790cbdcb5499ae08eeb14024c8de1
Lucas Agustin Morales Romero <moralesromerolucasagustin@gmail.com>
2024-06-20 10:07:15 -0300

    se crea el achivo controlador dentro de la carpeta corrspondiente

---
+++ backend/controllers/movieController.js
@@ -0,0 +1,11 @@
+//se genera una constante de base de datos 
+//src/controllers/movieController.js
+const db = require('../db/db');
+
+const getAllMovies = (req, res) => {
+    const sql = 'SELECT * FROM movies.pelicula';
+    db.query(sql, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+};
