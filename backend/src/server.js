2b77fee97fda83c5bb29f99133de37cc752a06f2
Lucas Agustin Morales Romero <moralesromerolucasagustin@gmail.com>
2024-06-20 10:14:18 -0300

    Se mueve el archivo server a la carpeta src

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+//requerimos express y las rutas a utilizar
+const express = require('express');
+const movieRoutes = require('../routes/movieRoutes');
+
+
+const app = express();
+
+app.use(express.json());
+app.use('/movies', movieRoutes);
+const PORT = 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+	});
