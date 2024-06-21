95fd034fc0300e71b0ac58687b219e2e4c25cd64
Lucas Agustin Morales Romero <moralesromerolucasagustin@gmail.com>
2024-06-21 09:54:12 -0300

    Se cambia la query a la base de datos

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -3,9 +3,9 @@
 const db = require('../db/db');
 
 const getAllMovies = (req, res) => {
-    const sql = 'SELECT * FROM movies';
+    const sql = 'SELECT * FROM movies.pelicula';
     db.query(sql, (err, results) => {
         if (err) throw err;
         res.json(results);
     }); 
-};
+};git 
