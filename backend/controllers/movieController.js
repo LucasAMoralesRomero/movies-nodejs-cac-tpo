c8d53dd34c96b56d5fc076e8792a15ac815bdbe1
Lucas Agustin Morales Romero <moralesromerolucasagustin@gmail.com>
2024-06-21 09:40:53 -0300

    SE realizan cambios en el controlador y en el archivo de rutas

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -3,9 +3,9 @@
 const db = require('../db/db');
 
 const getAllMovies = (req, res) => {
-    const sql = 'SELECT * FROM movies.pelicula';
+    const sql = 'SELECT * FROM movies';
     db.query(sql, (err, results) => {
         if (err) throw err;
         res.json(results);
-    });
+    }); 
 };
--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -5,12 +5,10 @@ const movieController = require('../controllers/movieController');
 //se definene las rutas para comenzar a trabajar con la entidad Movie
 //se obtinen todas las peliculas
 router.get('/', movieController.getAllMovies);
-//se obtienen las peliculas nuevas
-router.get('/', movieController.getAllNewMovies);
+
 //se obtiene una pelicula por id
 router.get('/:id', movieController.getMovieById);
-//se obtiene peliculas por genero
-router.get('/:genre'), movieController.getMovieByGenre;
+
 router.post('/', movieController.createMovie);
 router.put('/:id', movieController.updateMovie);
 router.delete('/:id', movieController.deleteMovie);
